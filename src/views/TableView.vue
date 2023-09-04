a0eca0170235912a3a8155a453778cdbea012010
yi <guiyc@outlook.com>
2023-09-04 16:12:43 +0800

    增加统计数值

--- src/views/TableView.vue
+++ src/views/TableView.vue
@@ -1,10 +1,24 @@
 <template>
-  <a-table
-    :columns="columns"
-    :data-source="data"
-    :pagination="{ pageSize: 50 }"
-    :scroll="{ y: 240 }"
-  />
+  <a-space direction="vertical">
+
+
+    <a-card>
+
+      <a-row>
+        <a-col :span="12">
+          <a-statistic title="Active Users" :value="112893" style="margin-right: 50px"
+            :value-style="{ color: '#3f8600' }" />
+        </a-col>
+        <a-col :span="12">
+          <a-statistic title="Account Balance (CNY)" :precision="2" :value="112893" />
+        </a-col>
+      </a-row>
+    </a-card>
+
+    <a-table :columns="columns" :data-source="data" :pagination="{ pageSize: 50 }"
+      :scroll="{ y: 240 }" />
+
+  </a-space>
 </template>
 <script lang="ts" setup>
 const columns = [
