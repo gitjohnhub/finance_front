32adca080d0fc74e01be5f0d7e1ead074488a464
yi <guiyc@outlook.com>
2023-09-04 15:40:39 +0800

    增加卡片视图

--- src/views/BoardView.vue
+++ src/views/BoardView.vue
@@ -7,6 +7,24 @@
       <a-statistic title="Account Balance (CNY)" :precision="2" :value="112893" />
     </a-col>
   </a-row>
+
+  <a-row :gutter="[16,16]">
+  <a-col :span="6">
+    <CardView></CardView>
+  </a-col>
+  <a-col :span="6">
+    <CardView></CardView>
+  </a-col>
+  <a-col :span="6">
+    <CardView></CardView>
+  </a-col>
+  <a-col :span="6">
+    <CardView></CardView>
+  </a-col>
+</a-row>
 </template>
+<script setup lang="ts">
+import CardView from '@/components/CardView.vue';
+</script>
 
 
